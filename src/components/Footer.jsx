8a2c3caa637b34de7711868d8cf889ecc1c2ab4f
Anirudh <anirudhx5@gmail.com>
2017-12-16 19:04:22 +0000

    redesigned Header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,7 @@ export default class Footer extends React.Component {
       return (
         <footer className="page-footer" id="contact">
           <div className="container">
-            <div className="row">
-              <h1 className="white-text">Contact Me</h1>
-              <p className="grey-text text-lighten-4">ani@ucl.ac.uk</p>
-              <p className="grey-text text-lighten-4">Thanks, check back later for more projects.</p>
-            </div>
+              <p>ani@ucl.ac.uk</p>
           </div>
         </footer>
       )
